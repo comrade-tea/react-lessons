2c565855210960bada509f36ab0feaf92185e8fe
Vasil <comrade.tea@gmail.com>
2018-03-02 18:44:36 +0300

    .

--- src/Article.js
+++ src/Article.js
@@ -1,13 +1,38 @@
 import React, {Component} from "react"
 
-export default class Article extends Component{
+export default class Article extends Component {
+
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isOpen: false
+		}
+	}
+
 	render() {
 		console.log("---", this.props);
 		const {article} = this.props;
 
 		return <div>
-					<h1>{article.title}</h1>
-					<div>{article.text}</div>
-				</div>
+			<h1>{article.title}</h1>
+			<button onClick={this.toggleOpen}>{this.state.isOpen ? "close" : "open"}</button>
+			{this.getBody()}
+		</div>
 	}
+
+	getBody() {
+		if (!this.state.isOpen)
+			return null;
+
+		return <div>{this.props.article.text}</div>
+	}
+
+	toggleOpen = () => {
+		this.setState({
+			isOpen: this.state.isOpen = !this.state.isOpen
+		})
+	}
+
 }
