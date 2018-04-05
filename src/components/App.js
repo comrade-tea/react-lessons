993b8cdfa1531b1705462100d74c0ae25362f491
Vasil <comrade.tea@gmail.com>
2018-04-05 17:38:09 +0300

    .

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,35 @@ import ArticleList from "./ArticleList"
 import ArticleChart from "./ArticleChart"
 import UserForm from "./UserForm";
 import Select from "react-select";
+import 'react-select/dist/react-select.css';
+
+import {articles} from "../fixtures";
 
 class App extends Component {
 
+	state = {
+		selection: null
+	}
+
 	render() {
-		const ninja = [{ value: 'one', label: 'One' },
-			{ value: 'two', label: 'Two' }];
+		const options = this.props.articles.map(item => ({
+			label: item.title,
+			value: item.id
+		}));
 
 		return (
 			<div>
 				<UserForm/>
-				<Select options={ninja}/>
+				<Select options={options} onChange={this.changeSelection} value={this.state.selection}/>
 				<ArticleList articles={this.props.articles}/>
 				<ArticleChart articles={this.props.articles}/>
 			</div>
 		);
+	}
+
+	changeSelection = selection => {
+		console.log("----", selection);
+		this.setState({selection});
 	}
 
 }
